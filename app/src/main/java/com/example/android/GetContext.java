419a9c45a06ecc57c1b62ddaf2c2c50f28c20a23
yqhxs <2325751238@qq.com>
2020-04-18 00:21:25 +0800

    login pages

---
+++ app/src/main/java/com/example/android/GetContext.java
@@ -0,0 +1,16 @@
+package com.example.android;
+
+import android.app.Application;
+import android.content.Context;
+
+public class GetContext extends Application {
+    private static Context context;
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        context = getApplicationContext();
+    }
+    public static Context getContext(){
+        return context;
+    }
+}
